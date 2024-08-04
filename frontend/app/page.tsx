5465ccc1571a7e7e3f0146188c9196e6864e9771
gdloparco <giandomenicoloparco@gmail.com>
2024-08-04 10:45:38 +0100

    docker containerisation

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-row items-center justify-center">
+    <div className="min-h-screen flex flex-col items-center justify-center">
       <Navbar />
 
       <main className="flex flex-col items-center justify-center py-20">
@@ -17,7 +17,7 @@ export default function Home() {
               height={96}
             />
           </div>
-          <h1 className="text-3xl font-bold">Some</h1>
+          <h1 className="text-3xl font-bold">God exists</h1>
           <p className="text-center max-w-md">Description of your homepage.</p>
           <button className="px-6 py-3 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600">
             Get Started
