63afbf6d26530153fb2dc2cabf5fa07ed465c063
gdloparco <giandomenicoloparco@gmail.com>
2024-08-06 12:31:27 +0100

    changes to navbar and landing page responsive

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,25 +1,24 @@
 import React from "react";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <nav className="sticky top-0 z-50 bg-white shadow-md w-full">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
-        <div className="flex justify-between items-center">
-          <div className="flex items-center space-x-4">
-            <div className="flex-shrink-0">
-              <img
-                className="h-8"
-                src="../logo-test.png"
-                alt="Logo"
+    <nav className="sticky top-0 z-50 bg-purple-900 shadow-md w-full">
+      <div className="mx-auto px-6 md:px-8 lg:px-10 py-6 lg:py-8">
+        <div className="flex items-center space-x-6">
+              <Image
+                className="sm:scale-100 md:scale-125 lg:scale-150"
+                src="/feather_yellow.png"
+                alt="small logo feather yellow"
+                width={30}
+                height={30}
               />
+            <div className="md:flex space-x-4 text-amber-400 text-center">
+              <a href="">Home</a>
             </div>
-            <div className="md:flex space-x-4">
-              <h1>1</h1>
+            <div className="md:flex space-x-4 text-amber-400 text-center">
+              <a href="">About</a>
             </div>
-            <div className="md:flex space-x-4">
-              <h1>3</h1>
-            </div>
-          </div>
         </div>
       </div>
     </nav>
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,21 +6,22 @@ export default function Home() {
   return (
     <div>
       <Navbar />
-        <div className="min-h-screen flex flex-col items-center justify-center">
+        <div className="bg-amber-300 min-h-screen flex flex-col items-center justify-center">
         <main className="flex flex-col items-center justify-center py-20">
           <div className="flex flex-col items-center space-y-6">
-            <div className="w-24 h-24">
+            <div className="md:py-5 lg:py-10">
               <Image
-                src="/logo-test.png"
-                alt="Small Logo"
-                width={96}
-                height={96}
+                className="sm:scale-100 md:scale-125 lg:scale-150"
+                src="/feather_purple.png"
+                alt="small logo feather purple"
+                width={100}
+                height={100}
               />
             </div>
-            <h1 className="text-3xl font-bold">Title</h1>
-            <p className="text-center max-w-md">Description of your homepage.</p>
-            <button className="px-6 py-3 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600">
-              Buon Appetito!
+            <h1 className="text-7xl font-bold text-purple-800">K</h1>
+            <p className="text-center max-w-md">Cover Letter Generator</p>
+            <button className="px-7 py-3 border-1 border-stone-800 bg-purple-800 text-amber-400 rounded-2xl shadow-lg hover:bg-purple-900">
+              Start Here
             </button>
           </div>
         </main>
