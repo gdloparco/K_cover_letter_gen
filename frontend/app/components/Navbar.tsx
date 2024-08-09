4eaef1ac07f8e4e405d365f928448152548b3a5e
gdloparco <giandomenicoloparco@gmail.com>
2024-08-09 12:01:37 +0100

    Navbar completed

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+"use client";
+import React, { useState } from 'react';
 import Image, { StaticImageData } from "next/image";
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 
 interface NavbarProps {
   logoImage: StaticImageData;
@@ -12,8 +14,9 @@ export default function Navbar({logoImage}: NavbarProps) {
     {name: 'Home', link: '/'},
     {name: 'About', link: '/'},
     {name: 'Contact', link: '/'},
-
   ]
+
+  let [isOpen, setisOpen] = useState(false)
 
   return (
     // <nav className="sticky top-0 z-50 bg-purple-900 shadow-md w-full">
@@ -35,7 +38,7 @@ export default function Navbar({logoImage}: NavbarProps) {
     //     </div>
     //   </div>
     // </nav>
-    <nav className="bg-purple-900 sticky top-0 z-50 w-full text-amber-300 shadow-2xl font-Kanit">
+    <nav className="bg-purple-900 sticky top-0 left-0 z-50 w-full text-amber-300 shadow-2xl font-Kanit">
       <div className="md:px-10 py-4 px-7 md:flex justify-between items-center">
 
         {/* Logo  */}
@@ -49,9 +52,20 @@ export default function Navbar({logoImage}: NavbarProps) {
             height={45}
           />
 
+        {/* Mobile Menu Icon  */}
+
+        <div onClick={() => setisOpen(!isOpen)} className='w-9 h-9 absolute right-8 top-6 cursor-pointer md:hidden'>
+          {
+            isOpen ? <XMarkIcon/> : <Bars3Icon/>
+          }
+        </div>
+
+
         {/* Nav Links  */}
 
-        <ul className='md:flex pl-9 md:pl-0 text-xl lg:text-2xl'>
+        <ul className={`
+          text-xl lg:text-2xl md:flex md:items-center pb-0 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in bg-purple-900
+          ${isOpen ? 'top-12' : 'top-[-350px]'}`}>
           {
             Links.map(link => (
             <li className='my-7 md:my-0 md:ml-8'>
@@ -63,6 +77,11 @@ export default function Navbar({logoImage}: NavbarProps) {
 
           {/* <div className="md:hidden flex items-center">
             <button className="mobile-menu-button">
+
+            md:flex pl-9 md:pl-10 lg:pl-16 text-xl lg:text-2xl md:items-center md:pb-0 pb-12 
+          absolute md:z-auto z-[-1] left-0 w-full transition-all duration-500 ease-in bg-purple-900
+
+
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="44"
