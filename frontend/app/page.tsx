d11809c2ee9500b097e7a4e301e9d73376149a36
gdloparco <giandomenicoloparco@gmail.com>
2024-09-02 14:29:52 +0100

    renaming homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
             <div className="flex flex-col items-center space-y-10 lg:space-y-16">
               <h1 className=" font-semibold text-6xl md:text-7xl lg:text-8xl text-purple-800">Kate</h1>
               <p className="px-20 lg:px-52 text-center text-xl md:text-2xl max-w-max text-stone-800">Crafting the perfect cover letter can be a time-consuming and daunting task, especially when applying to multiple job openings.</p>
-              <p className="px-20 lg:px-52 text-center text-xl md:text-2xl max-w-max text-stone-800">"K" is a user-friendly Cover Letter Generator that simplifies the process, allowing you to quickly generate personalized cover letters tailored to each job description, based on your preferences.</p>
+              <p className="px-20 lg:px-52 text-center text-xl md:text-2xl max-w-max text-stone-800">Kate is a user-friendly Cover Letter Generator that simplifies the process, allowing you to quickly generate personalized cover letters tailored to each job description, based on your preferences.</p>
               <button className="px-6 py-2 lg:py-3 border-2 border-stone-800 bg-purple-800 text-amber-300 rounded-2xl text-2xl md:text-3xl shadow-lg hover:bg-purple-900">
                 Start Here
               </button>
