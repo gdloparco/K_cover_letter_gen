41d6560168d4b918c2a39b982b8dff315c7898aa
gdloparco <giandomenicoloparco@gmail.com>
2024-09-09 19:05:16 +0100

    cleanup

--- frontend/app/api/email/route.ts
+++ frontend/app/api/email/route.ts
@@ -1,22 +1,21 @@
-import { type NextRequest, NextResponse } from 'next/server';
-import nodemailer from 'nodemailer';
-import Mail from 'nodemailer/lib/mailer';
+import { type NextRequest, NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+import Mail from "nodemailer/lib/mailer";
 
 export async function POST(request: NextRequest) {
   const { email, name, message } = await request.json();
 
   const transport = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
       user: process.env.MY_EMAIL,
       pass: process.env.MY_PASSWORD,
     },
     tls: {
-        rejectUnauthorized: false, // This allows self-signed certificates
+      // This allows self-signed certificates
+      rejectUnauthorized: false,
     },
   });
-
-  console.log(process.env.MY_EMAIL, process.env.MY_PASSWORD);
 
   const mailOptions: Mail.Options = {
     from: process.env.MY_EMAIL,
@@ -30,7 +29,7 @@ export async function POST(request: NextRequest) {
     new Promise<string>((resolve, reject) => {
       transport.sendMail(mailOptions, function (err) {
         if (!err) {
-          resolve('Email sent');
+          resolve("Email sent");
         } else {
           reject(err.message);
         }
@@ -39,9 +38,9 @@ export async function POST(request: NextRequest) {
 
   try {
     await sendMailPromise();
-    return NextResponse.json({ message: 'Email sent' });
+    return NextResponse.json({ message: "Email sent" });
   } catch (err) {
-    console.error('Error somewhere sending email:', err);
+    console.error("Error somewhere sending email:", err);
     return NextResponse.json({ error: err }, { status: 500 });
   }
 }
