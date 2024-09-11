f1579f3c343a7c26c5b0c147b34c3e5d07528c50
gdloparco <giandomenicoloparco@gmail.com>
2024-09-11 15:37:52 +0100

    Form fully responsive with uniform styling

--- frontend/app/contact/page.tsx
+++ frontend/app/contact/page.tsx
@@ -12,61 +12,61 @@ export default function Contact() {
       <Navbar logoImage={logoYellow} />
       <div className="bg-amber-300 min-h-svh flex flex-col font-Kanit">
         <main className="flex flex-col items-center py-10">
-            <div className="md:py-5 lg:py-10 md:mb-6 w-full transition-all duration-500 ease-in md:static md:z-auto">
-              <div className="flex flex-col items-center justify-center py-10 gap-8">
-                <p className="text-center text-2xl md:text-3xl lg:text-4xl max-w-max text-stone-800 transition-all duration-500 ease-in md:static md:z-auto">
-                  Interested in this project?
+          <div className="md:py-5 lg:py-10 md:mb-6 w-full transition-all duration-500 ease-in md:static md:z-auto">
+            <div className="flex flex-col items-center justify-center py-10 gap-8">
+              <p className="text-center text-2xl md:text-3xl lg:text-4xl max-w-max text-stone-800 transition-all duration-500 ease-in md:static md:z-auto">
+                Interested in this project?
+              </p>
+              <p className="text-center text-2xl md:text-3xl lg:text-4xl max-w-max text-stone-800 transition-all duration-500 ease-in md:static md:z-auto">
+                Get in touch!
+              </p>
+            </div>
+            <div className="flex flex-col md:flex-row px-20 py-4 md:gap-4 justify-between md:justify-around w-full">
+              <div className="flex flex-row items-center justify-center py-4 gap-4 md:gap-6 lg:gap-8 transition-all duration-500 ease-in md:static md:z-auto">
+                <p className="text-center text-xl md:text-2xl lg:text-3xl max-w-max text-stone-800 transition-all duration-500 ease-in md:static md:z-auto">
+                  LinkedIn
                 </p>
-                <p className="text-center text-2xl md:text-3xl lg:text-4xl max-w-max text-stone-800 transition-all duration-500 ease-in md:static md:z-auto">
-                  Get in touch!
-                </p>
+                <a href="https://www.linkedin.com/in/dom-loparco/">
+                  <FontAwesomeIcon
+                    className="md:scale-125 lg:scale-150"
+                    icon={faLinkedin}
+                    style={{ color: "#0a66c2" }}
+                    width={35}
+                    height={35}
+                  />
+                </a>
               </div>
-              <div className="flex flex-col md:flex-row px-20 py-4 md:gap-4 justify-between md:justify-around w-full">
-                <div className="flex flex-row items-center justify-center py-4 gap-4 md:gap-6 lg:gap-8 transition-all duration-500 ease-in md:static md:z-auto">
-                  <p className="text-center text-xl md:text-2xl lg:text-3xl max-w-max text-stone-800 transition-all duration-500 ease-in md:static md:z-auto">
-                    LinkedIn
-                  </p>
-                  <a href="https://www.linkedin.com/in/dom-loparco/">
-                    <FontAwesomeIcon
-                      className="md:scale-125 lg:scale-150"
-                      icon={faLinkedin}
-                      style={{ color: "#0a66c2" }}
-                      width={35}
-                      height={35}
-                    />
-                  </a>
-                </div>
-                <div className="flex flex-row items-center justify-center py-4 gap-4 md:gap-6 lg:gap-8 transition-all duration-500 ease-in md:static md:z-auto">
-                  <p className="text-center text-xl md:text-2xl lg:text-3xl max-w-max text-stone-800 transition-all duration-500 ease-in md:static md:z-auto">
-                    gdloparco.com
-                  </p>
-                  <a href="https://www.gdloparco.com/">
-                    <Image
-                      className="md:scale-125 lg:scale-150"
-                      src="/website-logo.png"
-                      alt="small dom website logo"
-                      width={35}
-                      height={35}
-                    />
-                  </a>
-                </div>
-                <div className="flex flex-row items-center justify-center py-4 gap-4 md:gap-6 lg:gap-8 transition-all duration-500 ease-in md:static md:z-auto">
-                  <p className="text-center text-xl md:text-2xl lg:text-3xl max-w-max text-stone-800 transition-all duration-500 ease-in md:static md:z-auto">
-                    GitHub
-                  </p>
-                  <a href="https://github.com/gdloparco/">
-                    <FontAwesomeIcon
-                      className="md:scale-125 lg:scale-150"
-                      icon={faSquareGithub}
-                      style={{ color: "#000000" }}
-                      width={35}
-                      height={35}
-                    />
-                  </a>
-                </div>
+              <div className="flex flex-row items-center justify-center py-4 gap-4 md:gap-6 lg:gap-8 transition-all duration-500 ease-in md:static md:z-auto">
+                <p className="text-center text-xl md:text-2xl lg:text-3xl max-w-max text-stone-800 transition-all duration-500 ease-in md:static md:z-auto">
+                  gdloparco.com
+                </p>
+                <a href="https://www.gdloparco.com/">
+                  <Image
+                    className="md:scale-125 lg:scale-150"
+                    src="/website-logo.png"
+                    alt="small dom website logo"
+                    width={35}
+                    height={35}
+                  />
+                </a>
+              </div>
+              <div className="flex flex-row items-center justify-center py-4 gap-4 md:gap-6 lg:gap-8 transition-all duration-500 ease-in md:static md:z-auto">
+                <p className="text-center text-xl md:text-2xl lg:text-3xl max-w-max text-stone-800 transition-all duration-500 ease-in md:static md:z-auto">
+                  GitHub
+                </p>
+                <a href="https://github.com/gdloparco/">
+                  <FontAwesomeIcon
+                    className="md:scale-125 lg:scale-150"
+                    icon={faSquareGithub}
+                    style={{ color: "#000000" }}
+                    width={35}
+                    height={35}
+                  />
+                </a>
               </div>
             </div>
             <ContactForm />
+          </div>
         </main>
       </div>
     </div>
--- frontend/components/ContactForm.tsx
+++ frontend/components/ContactForm.tsx
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
-import { FC } from 'react';
-import { useForm } from 'react-hook-form';
-import { sendEmail } from '@/utils/send-email';
+import { FC } from "react";
+import { useForm } from "react-hook-form";
+import { sendEmail } from "@/utils/send-email";
 
 export type FormData = {
   name: string;
@@ -19,55 +19,57 @@ const ContactForm: FC = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className='mb-5'>
-        <label
-          htmlFor='name'
-          className='mb-3 block text-base font-medium text-black'
-        >
-          Full Name
-        </label>
-        <input
-          type='text'
-          placeholder='Full Name'
-          className='w-full rounded-md border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md'
-          {...register('name', { required: true })}
-        />
-      </div>
-      <div className='mb-5'>
-        <label
-          htmlFor='email'
-          className='mb-3 block text-base font-medium text-black'
-        >
-          Email Address
-        </label>
-        <input
-          type='email'
-          placeholder='example@domain.com'
-          className='w-full rounded-md border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md'
-          {...register('email', { required: true })}
-        />
-      </div>
-      <div className='mb-5'>
-        <label
-          htmlFor='message'
-          className='mb-3 block text-base font-medium text-black'
-        >
-          Message
-        </label>
-        <textarea
-          rows={4}
-          placeholder='Type your message'
-          className='w-full resize-none rounded-md border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md'
-          {...register('message', { required: true })}
-        ></textarea>
-      </div>
-      <div>
-        <button className='hover:shadow-form rounded-md bg-purple-500 py-3 px-8 text-base font-semibold text-white outline-none'>
-          Submit
-        </button>
-      </div>
-    </form>
+    <div className="bg-amber-300 mt-12 md:mt-20 lg:mt-28 flex flex-col md:block lg:px-80 md:px-56 items-center font-Kanit transition-all duration-500 ease-in md:static md:z-auto">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-5">
+          <label
+            htmlFor="name"
+            className="mb-3 block text-lg md:text-xl lg:text-2xl font-medium text-stone-800"
+          >
+            Full Name
+          </label>
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="w-full rounded-md border border-gray-300 bg-white py-3 px-6 text-lg md:text-xl lg:text-2xl font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md"
+            {...register("name", { required: true })}
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="email"
+            className="mb-3 block text-lg md:text-xl lg:text-2xl font-medium text-stone-800"
+          >
+            Email Address
+          </label>
+          <input
+            type="email"
+            placeholder="example@domain.com"
+            className="w-full rounded-md border border-gray-300 bg-white py-3 px-6 text-lg md:text-xl lg:text-2xl font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md"
+            {...register("email", { required: true })}
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="message"
+            className="mb-3 block text-lg md:text-xl lg:text-2xl font-medium text-stone-800"
+          >
+            Message
+          </label>
+          <textarea
+            rows={4}
+            placeholder="Type your message"
+            className="w-full resize-none rounded-md border border-gray-300 bg-white py-3 px-6 text-lg md:text-xl lg:text-2xl font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md"
+            {...register("message", { required: true })}
+          ></textarea>
+        </div>
+        <div className="flex items-center justify-center">
+          <button className="px-6 py-2 md:mt-6 border-2 border-stone-800 bg-purple-800 text-amber-300 rounded-2xl text-xl md:text-xl lg:text-2xl shadow-lg hover:bg-purple-900">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
