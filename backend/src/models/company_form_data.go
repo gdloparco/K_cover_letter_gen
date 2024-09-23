a45b8f7c59783d98a541ce8d4fc8692455900039
gdloparco <giandomenicoloparco@gmail.com>
2024-09-23 16:27:40 +0100

    backend models updated to hold response from Python Scraper

--- backend/src/models/company_form_data.go
+++ backend/src/models/company_form_data.go
@@ -6,10 +6,18 @@ import (
 
 type ProcessedCompanyData struct {
 	gorm.Model
-	Username             string `json:"username"`
-	CompanyName          string `json:"company_name"`
-	CompanyWebsite       string `json:"company_website"`
-	JobDescription       string `json:"job_description"`
-	JobDescriptionValues string `json:"job_description_values"`
-	WebsiteValues        string `json:"website_values"`
+	Username             string   `json:"username"`
+	CompanyName          string   `json:"company_name"`
+	CompanyWebsite       string   `json:"company_website_url"`
+	JobDescription       string   `json:"job_description"`
+	JobDescriptionValues string   `json:"job_description_values"`
+	WebsiteValues        string   `json:"website_values"`
+	SearchedLinks        []string `json:"searched_links"`
+}
+
+type PythonScraperResponse struct {
+	CompanyName      string   `json:"company_name"`
+	CompanyWebsite   string   `json:"company_website_url"`
+	RawCompanyValues string   `json:"extracted_values"`
+	SearchedLinks    []string `json:"searched_links"`
 }
